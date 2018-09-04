fd60f17ad59c282de8d961a5981ba911b558bde2
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-09-04 14:08:54 +0200

    Implement Card class

--- src/main/com/java/battleoftrams/model/Card.java
+++ src/main/com/java/battleoftrams/model/Card.java
@@ -1,5 +1,47 @@
 package main.com.java.battleoftrams.model;
 
-public class Card{
+import main.com.java.battleoftrams.controler.Game;
+
+public abstract class Card implements Comparable{
+    private int travelTime;
+    private int zulCount;
+    private int driverRudeness;
+    private int kanarCount;
+
+    Card(int travelTime, int zulCount, int driverRudeness, int kanarCount) {
+        this.travelTime = travelTime;
+        this.zulCount = zulCount;
+        this.driverRudeness = driverRudeness;
+        this.kanarCount = kanarCount;
+    }
+
+    public int getTravelTime() {
+        return travelTime;
+    }
+
+    public int getZulCunt() {
+        return zulCount;
+    }
+
+    public int getDriverRudeness() {
+        return driverRudeness;
+    }
+
+    public int getKanarCount() {
+        return kanarCount;
+    }
+    
+    @Override
+    public int compareTo(Card card) {
+        if (Game.getSelectedCharasteristic.equals("travelTime")) {
+            return this.travelTime - card.travelTime;
+        } else if (Game.getSelectedCharasteristic.equals("zulCount")) {
+            return this.zulCount - card.zulCount;
+        } else if (Game.getSelectedCharasteristic.equals("driverRudeness")) {
+            return this.driverRudeness - card.driverRudeness;
+        } else if (Game.getSelectedCharasteristic.equals("kanarCount")) {
+            return this.kanarCount - card.kanarCount;
+        }
+    }
     
 }
