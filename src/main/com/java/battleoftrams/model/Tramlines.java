a2de438626b08144bbfd84fd8e12bad064bd47b0
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-09-04 15:56:08 +0200

    Correct enum values references

--- src/main/com/java/battleoftrams/model/Tramlines.java
+++ src/main/com/java/battleoftrams/model/Tramlines.java
@@ -36,40 +36,40 @@ public enum Tramlines {
 
         if (tramType == Tramlines.Line_8) {
             characteristics[0] = ThreadLocalRandom.current()
-                    .nextInt(Line_8.minTravelTime, Line_8.maxTravelTime + 1);
+                    .nextInt(Tramlines.Line_8.minTravelTime, Tramlines.Line_8.maxTravelTime + 1);
             characteristics[1] = ThreadLocalRandom.current()
-                    .nextInt(Line_8.minZulCount, Line_8.maxZulCount + 1);
+                    .nextInt(Tramlines.Line_8.minZulCount, Tramlines.Line_8.maxZulCount + 1);
             characteristics[2] = ThreadLocalRandom.current()
-                    .nextInt(Line_8.minDriverRudeness, Line_8.maxDriverRudeness + 1);
+                    .nextInt(Tramlines.Line_8.minDriverRudeness, Tramlines.Line_8.maxDriverRudeness + 1);
             characteristics[3] = ThreadLocalRandom.current()
-                    .nextInt(Line_8.minKanarCount, Line_8.maxKanarCount + 1);
+                    .nextInt(Tramlines.Line_8.minKanarCount, Tramlines.Line_8.maxKanarCount + 1);
         } else if (tramType == Tramlines.Line_10) {
             characteristics[0] = ThreadLocalRandom.current()
-                    .nextInt(Line_10.minTravelTime, Line_10.maxTravelTime + 1);
+                    .nextInt(Tramlines.Line_10.minTravelTime, Tramlines.Line_10.maxTravelTime + 1);
             characteristics[1] = ThreadLocalRandom.current()
-                    .nextInt(Line_10.minZulCount, Line_10.maxZulCount + 1);
+                    .nextInt(Tramlines.Line_10.minZulCount, Tramlines.Line_10.maxZulCount + 1);
             characteristics[2] = ThreadLocalRandom.current()
-                    .nextInt(Line_10.minDriverRudeness, Line_10.maxDriverRudeness + 1);
+                    .nextInt(Tramlines.Line_10.minDriverRudeness, Tramlines.Line_10.maxDriverRudeness + 1);
             characteristics[3] = ThreadLocalRandom.current()
-                    .nextInt(Line_10.minKanarCount, Line_10.maxKanarCount + 1);
+                    .nextInt(Tramlines.Line_10.minKanarCount, Tramlines.Line_10.maxKanarCount + 1);
         } else if (tramType == Tramlines.Line_19) {
             characteristics[0] = ThreadLocalRandom.current()
-                    .nextInt(Line_19.minTravelTime, Line_19.maxTravelTime + 1);
+                    .nextInt(Tramlines.Line_19.minTravelTime, Tramlines.Line_19.maxTravelTime + 1);
             characteristics[1] = ThreadLocalRandom.current()
-                    .nextInt(Line_19.minZulCount, Line_19.maxZulCount + 1);
+                    .nextInt(Tramlines.Line_19.minZulCount, Tramlines.Line_19.maxZulCount + 1);
             characteristics[2] = ThreadLocalRandom.current()
-                    .nextInt(Line_19.minDriverRudeness, Line_19.maxDriverRudeness + 1);
+                    .nextInt(Tramlines.Line_19.minDriverRudeness, Tramlines.Line_19.maxDriverRudeness + 1);
             characteristics[3] = ThreadLocalRandom.current()
-                    .nextInt(Line_19.minKanarCount, Line_19.maxKanarCount + 1);
+                    .nextInt(Tramlines.Line_19.minKanarCount, Tramlines.Line_19.maxKanarCount + 1);
         } else if (tramType == Tramlines.Line_24) {
             characteristics[0] = ThreadLocalRandom.current()
-                    .nextInt(Line_24.minTravelTime, Line_24.maxTravelTime + 1);
+                    .nextInt(Tramlines.Line_24.minTravelTime, Tramlines.Line_24.maxTravelTime + 1);
             characteristics[1] = ThreadLocalRandom.current()
-                    .nextInt(Line_24.minZulCount, Line_24.maxZulCount + 1);
+                    .nextInt(Tramlines.Line_24.minZulCount, Tramlines.Line_24.maxZulCount + 1);
             characteristics[2] = ThreadLocalRandom.current()
-                    .nextInt(Line_24.minDriverRudeness, Line_24.maxDriverRudeness + 1);
+                    .nextInt(Tramlines.Line_24.minDriverRudeness, Tramlines.Line_24.maxDriverRudeness + 1);
             characteristics[3] = ThreadLocalRandom.current()
-                    .nextInt(Line_24.minKanarCount, Line_24.maxKanarCount + 1);
+                    .nextInt(Tramlines.Line_24.minKanarCount, Tramlines.Line_24.maxKanarCount + 1);
         }
         return characteristics;
     }
