7c13dcf407930e25d7c26f0b18e5726205192ec8
Michal Wisniewski <mihuwis@gmail.com>
2018-09-04 15:00:36 +0200

    Add printStatus

--- src/main/com/java/battleoftrams/view/View.java
+++ src/main/com/java/battleoftrams/view/View.java
@@ -1,8 +1,28 @@
 package main.com.java.battleoftrams.view;
 
+import java.util.List;
+
+import main.com.java.battleoftrams.model.Player;
+
 public class View{
+    private String whiteSpace = " ";
+    private String border = "*";
+    private String leftBracket = "(";
+    private String rightBracket = ")";
+    private int numberOfPlayers = 4;
 
     public void printMenu(){
         System.out.println("Menu:\n\t 1. Play\n\t 2. How to play\n\t 3. Quit");
+    }
+
+    public void printStatus(List<Player> listOfPlayers){
+        String status = "";
+        for (int i = 0; i < numberOfPlayers; i++){
+            status.format("%10s %10s %s %2d %s %10s ", 
+            whiteSpace, listOfPlayers.get(i).getName(), 
+            leftBracket, listOfPlayers.get(i).getListSize(), 
+            rightBracket, whiteSpace);
+        }
+        System.out.println(status);
     }
 }
