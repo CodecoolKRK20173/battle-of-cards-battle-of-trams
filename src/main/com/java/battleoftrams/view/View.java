d136595e5204516836ebe4bff929cd5745f710fd
Michal Wisniewski <mihuwis@gmail.com>
2018-09-04 12:09:39 +0200

    Add print menu

--- src/main/com/java/battleoftrams/view/View.java
+++ src/main/com/java/battleoftrams/view/View.java
@@ -3,6 +3,6 @@ package main.com.java.battleoftrams.view;
 public class View{
 
     public void printMenu(){
-        System.out.println("Menu");
+        System.out.println("Menu:\n\t 1. Play\n\t 2. How to play\n\t 3. Quit");
     }
 }
