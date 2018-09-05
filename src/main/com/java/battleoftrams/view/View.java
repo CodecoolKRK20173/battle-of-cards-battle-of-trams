b5bd44634836ff4b5f895d961203dcba4373786a
Michal Wisniewski <mihuwis@gmail.com>
2018-09-05 09:05:35 +0200

    Add print Cards - first part

--- src/main/com/java/battleoftrams/view/View.java
+++ src/main/com/java/battleoftrams/view/View.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 import main.com.java.battleoftrams.model.Player;
 
+//    Name1 (15)       Name2 (15)       Name3 (15)       Name4 (15) 
+// ***************  ***************  ***************  ***************
+// * Line   24   *  * Line   10   *  * Line   19   *  * Line   08   *
+// ***************  ***************  ***************  ***************
+// * Time   40   *  * Time   60   *  * Time   45   *  * Time   75   *
+// * Żule   05   *  * Żule   12   *  * Żule   07   *  * Żule   09   *
+// * MRF    34   *  * MRF    34   *  * MRF    34   *  * MRF    34   *
+// * Kanary 12   *  * Kanary 08   *  * Kanary 12   *  * Kanary 12   *
+// ***************  ***************  ***************  ***************
+
 public class View{
     private String whiteSpace = " ";
     private String border = "*";
@@ -18,11 +28,22 @@ public class View{
     public void printStatus(List<Player> listOfPlayers){
         String status = "";
         for (int i = 0; i < numberOfPlayers; i++){
-            status.format("%10s %10s %s %2d %s %10s ", 
+            status += String.format("%s %5s %s %2d %s %4s ", 
             whiteSpace, listOfPlayers.get(i).getName(), 
             leftBracket, listOfPlayers.get(i).getListSize(), 
             rightBracket, whiteSpace);
         }
         System.out.println(status);
+    }
+
+    public void printCards(List<Player> listOfPlayers){
+        StringBuilder table = StringBuilder();
+        table.append(cardBorders()).append("\n");
+        table.append(border).append(" ").append(listOfPlayers.get(0).getClass().toString()).append("  ").append(border).append(" ");
+        
+    }
+
+    private String cardBorders(){
+        return String.format("%15s   %15s   %15s    %15s", border, border, border, border);
     }
 }
