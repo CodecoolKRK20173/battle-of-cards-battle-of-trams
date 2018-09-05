61950693c0bed85234b2f843cb1d06a10b609ca6
Marta Zając <xmartza1@gmail.com>
2018-09-05 10:27:15 +0200

    Implement classes Player, Human, Computer

--- src/main/com/java/battleoftrams/model/Computer.java
+++ src/main/com/java/battleoftrams/model/Computer.java
@@ -1,5 +1,20 @@
 package main.com.java.battleoftrams.model;
 
-public class Computer{
+import java.util.List;
+import java.util.Random;
+
+public class Computer extends Player{
+
+    public Computer(){
+        super();
+    }
+
+    @Override
+    public String selectCharacteristic(){
+        List <String> characteristicsList = Arrays.asList("time", "żulCounter", "driverRudeness", "canarCounter");
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(4);
+        characteristicsList.get(randomIndex);
+    }
     
 }
--- src/main/com/java/battleoftrams/model/Human.java
+++ src/main/com/java/battleoftrams/model/Human.java
@@ -1,5 +1,14 @@
 package main.com.java.battleoftrams.model;
 
-public class Human{
+public class Human extends Player{
+
+    public Human(){
+        super();
+    }
+
+    @Override
+    public String selectCharacteristic(){
+        return selectedCharacteristic;
+    }
     
 }
